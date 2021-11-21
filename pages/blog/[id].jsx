9e5138f226f38e51e59beedf618ad1dc0c9e8d8e
Alexander Celis <dacelis0@misena.edu.co>
2021-11-21 15:05:22 -0500

    delete blog path

--- pages/blog/[id].jsx
+++ pages/blog/[id].jsx
@@ -1,5 +1,39 @@
-const Post = () => {
-  return <h1>Post</h1>;
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  // map data to an array of path objects with params (id)
+  const paths = data.map(({ id }) => {
+    return {
+      params: { id: id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
-export default Post;
+export const getStaticProps = async ({ params }) => {
+  const id = params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { post: data },
+  };
+};
+
+const Details = ({ post }) => {
+  return (
+    <div>
+      <h1>{post.name}</h1>
+      <p>{post.email}</p>
+      <p>{post.website}</p>
+      <p>{post.address.city}</p>
+    </div>
+  );
+};
+
+export default Details;
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,5 +1,29 @@
-const Blog = () => {
-  return <h1>Blog</h1>;
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { users: data },
+  };
 };
 
-export default Blog;
+const Ninjas = ({ users }) => {
+  // console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {users.map(({ id, name }) => (
+        <Link href={"/blog/" + id} key={id}>
+          <a>
+            <h3>{name}</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Ninjas;
